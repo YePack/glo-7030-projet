7dc8de442c4ace241b8f737dd263e4ffe57e1601
Stephane Caron <ste.caron@icloud.com>
2019-03-16 15:06:38 -0400

    Ajout d'une fonction pour parser le meta data d'un task d'annotation

--- src/parser/xml_parser.py
+++ src/parser/xml_parser.py
@@ -2,7 +2,7 @@ import xml.etree.ElementTree as ET
 import os
 
 
-def parse_xml(path):
+def parse_xml_data(path):
     tree = ET.parse(path)
     root = tree.getroot()
 
@@ -27,6 +27,24 @@ def parse_xml(path):
     return labels, points
 
 
+def parse_xml_metadata(path):
+    tree = ET.parse(path)
+    root = tree.getroot()
+
+    task_name = root.findall('meta')[0][0][1].text
+    nb_images = root.findall('meta')[0][0][2].text
+    dumped_time = root.findall('meta')[0][1].text
+    user = root.findall('meta')[0][0][12][0].text
+
+    return {'task_name': task_name,
+            'nb_images': nb_images,
+            'dumped_time': dumped_time,
+            'user': user}
+
+
+# Test
 path_xml = os.path.expanduser('./data/xml/test1.xml')
-labels, points = parse_xml(path_xml)
+labels, points = parse_xml_data(path_xml)
+parse_xml_metadata(path_xml)
+
 
