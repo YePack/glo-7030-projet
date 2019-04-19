1547031513eb9cebdec9dc1fadda2774de21a488
Stephane Caron <ste.caron@icloud.com>
2019-04-18 21:18:06 -0400

    Add a function to split xml of mulitple images into separate xml

--- src/parser/xml_parser.py
+++ src/parser/xml_parser.py
@@ -1,5 +1,4 @@
 import xml.etree.ElementTree as ET
-import os
 
 
 def parse_xml_data(path):
@@ -32,19 +31,12 @@ def parse_xml_metadata(path):
     root = tree.getroot()
 
     task_name = root.findall('meta')[0][0][1].text
-    nb_images = root.findall('meta')[0][0][2].text
     dumped_time = root.findall('meta')[0][1].text
     user = root.findall('meta')[0][0][12][0].text
 
     return {'task_name': task_name,
-            'nb_images': nb_images,
             'dumped_time': dumped_time,
             'user': user}
 
-
-# Test
-#path_xml = os.path.expanduser('./data/xml/test1.xml')
-#labels, points = parse_xml_data(path_xml)
-#parse_xml_metadata(path_xml)
 
 
