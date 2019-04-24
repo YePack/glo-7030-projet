e9de85525dab9fd0a01901d22576603089fea20f
Stephane Caron <ste.caron@icloud.com>
2019-04-24 17:53:29 -0400

    Create a functuin for flip_images and modify the training functions

--- src/dataloader/flip_images.py
+++ src/dataloader/flip_images.py
@@ -5,28 +5,28 @@ from PIL import Image
 from torchvision import transforms
 from src.unet.utils import readfile, savefile
 
-path_data = 'data/train/'
+
+def flip_images(path_data):
+
+    images = glob.glob(path_data + '*.png')
+    labels = glob.glob(path_data + '*.pkl')
+
+    preprocess_flip = transforms.Compose([
+            transforms.RandomHorizontalFlip(1)
+    ])
+
+    for image in images:
+        image_flip = np.array(Image.open(image))[..., :3]
+        image_flip = Image.fromarray(image_flip)
+
+        image_flip = preprocess_flip(image_flip)
+        image_flip.save(image.replace('image', 'rimage'))
 
 
-images = glob.glob(path_data + '*.png')
-labels = glob.glob(path_data + '*.pkl')
+    for label in labels:
+        label_flip = readfile(label.replace('.pkl', ''))
+        label_flip = Image.fromarray(label_flip)
 
-preprocess_flip = transforms.Compose([
-        transforms.RandomHorizontalFlip(1)
-])
-
-for image in images:
-    image_flip = np.array(Image.open(image))[..., :3]
-    image_flip = Image.fromarray(image_flip)
-
-    image_flip = preprocess_flip(image_flip)
-    image_flip.save(image.replace('image', 'rimage'))
-
-
-for label in labels:
-    label_flip = readfile(label.replace('.pkl', ''))
-    label_flip = Image.fromarray(label_flip)
-
-    label_flip = preprocess_flip(label_flip)
-    label_flip = np.array(label_flip)
-    savefile(label_flip, label.replace('.pkl', '').replace('image', 'rimage'))
+        label_flip = preprocess_flip(label_flip)
+        label_flip = np.array(label_flip)
+        savefile(label_flip, label.replace('.pkl', '').replace('image', 'rimage'))
--- src/training_function.py
+++ src/training_function.py
@@ -11,26 +11,29 @@ from torch.utils.data import DataLoader
 from src.dataloader import DataGenerator
 
 
-def train_valid_loaders(train_path, batch_size, transform, train_val_split, shuffle=True):
+def train_valid_loaders(train_path, valid_path, batch_size, transform, shuffle=True):
 
-    # Split train and valid list of files
-    images = glob.glob(train_path + '*.png')
-    labels = glob.glob(train_path + '*.pkl')
-    images.sort()
-    labels.sort()
-    nb_images = len(images)
-    indices = np.arange(nb_images)
+    # List the files in train and valid
+    train_images = glob.glob(train_path + '*.png')
+    train_labels = glob.glob(train_path + '*.pkl')
+    valid_images = glob.glob(valid_path + '*.png')
+    valid_labels = glob.glob(valid_path + '*.pkl')
 
-    if shuffle:
-        np.random.shuffle(indices)
+    #images.sort()
+    #labels.sort()
+    #nb_images = len(images)
+    #indices = np.arange(nb_images)
 
-    split = math.floor(train_val_split * nb_images)
-    train_idx, valid_idx = indices[:split], indices[split:]
+    #if shuffle:
+        #np.random.shuffle(indices)
 
-    train_images = [images[i] for i in train_idx]
-    train_labels = [labels[i] for i in train_idx]
-    valid_images = [images[i] for i in valid_idx]
-    valid_labels = [labels[i] for i in valid_idx]
+    #split = math.floor(train_val_split * nb_images)
+    #train_idx, valid_idx = indices[:split], indices[split:]
+
+    #train_images = [images[i] for i in train_idx]
+    #train_labels = [labels[i] for i in train_idx]
+    #valid_images = [images[i] for i in valid_idx]
+    #valid_labels = [labels[i] for i in valid_idx]
 
     dataset_train = DataGenerator(train_images, train_labels, transform)
     dataset_val = DataGenerator(valid_images, valid_labels, transform)
@@ -71,13 +74,13 @@ def validate(model, val_loader, use_gpu=False):
     return sum(val_loss) / len(val_loss)
 
 
-def train(model, optimizer, train_path, n_epoch, batch_size, transform, train_val_split, use_gpu=False,
+def train(model, optimizer, train_path, valid_path, n_epoch, batch_size, transform, use_gpu=False,
           scheduler=None, criterion=None, shuffle=True, weight_adaptation=None):
 
     if criterion is None:
         criterion = nn.CrossEntropyLoss()
 
-    train_loader, val_loader = train_valid_loaders(train_path, transform=transform, train_val_split=train_val_split,
+    train_loader, val_loader = train_valid_loaders(train_path, valid_path, transform=transform,
                                                    batch_size=batch_size, shuffle=shuffle)
 
     for i in range(n_epoch):
