0cc3a735ed390d479af0c10f08df30661b43660b
Philippe Blouin-Leclerc <philippe.blouin-leclerc.1@ulaval.ca>
2019-04-26 13:54:44 -0400

    DiceLoss essaie

--- src/loss/dice_loss.py
+++ src/loss/dice_loss.py
@@ -11,11 +11,11 @@ class DiceCoeff(nn.Module):
         y_true_one_hot.cuda()
         y_pred_softmax = F.softmax(y_pred, dim=1)
         y_pred_softmax.cuda()
-        epsilon = torch.FloatTensor(0.000001).cuda()
+        epsilon = torch.FloatTensor([0.000001]).cuda()
         axes = tuple(range(2, len(y_pred_softmax.shape)))
-        numerator = (torch.FloatTensor(2).cuda() * (y_pred_softmax * y_true_one_hot).sum(dim=axes)).cuda()
+        numerator = (torch.FloatTensor([2]).cuda() * (y_pred_softmax * y_true_one_hot).sum(dim=axes)).cuda()
         denominator = y_pred_softmax.sum(dim=axes).cuda() + y_true_one_hot.sum(dim=axes).cuda()
-        return torch.FloatTensor(1).cuda() - (numerator / (denominator + epsilon)).mean()
+        return torch.FloatTensor([1]).cuda() - (numerator / (denominator + epsilon)).mean()
 
     @staticmethod
     def _one_hot(input, nb_class=9):
