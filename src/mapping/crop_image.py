3be121aee3814c9369ef77937b081e4148361811
Philippe Blouin-Leclerc <philippe.blouin-leclerc.1@ulaval.ca>
2019-11-20 19:44:00 -0500

    fct to crop

---
+++ src/mapping/crop_image.py
@@ -0,0 +1,30 @@
+import glob
+import numpy as np
+from PIL import Image
+from pathlib import PurePath
+import matplotlib
+
+IMAGE_PATH = 'data/train/'
+filenames = glob.glob(IMAGE_PATH + '*.png')
+
+
+def load_image(file):
+    return Image.open(file)
+
+
+def crop_center(img, cropx, cropy):
+    y, x = img.shape[0:2]
+    startx = x // 2 - (cropx // 2)
+    starty = y // 2 - (cropy // 2)
+    return img[starty:starty + cropy, startx:startx + cropx]
+
+
+for filename in filenames:
+    with open(filename, 'rb') as f:
+        image = np.array(load_image(f))
+        new_image = crop_center(image, 450, 256)
+
+        pure_path = PurePath(filename)
+        crop_name = pure_path.name[:-4] + '_crop.png'
+        path_crop_name = str(pure_path.with_name(crop_name))
+        matplotlib.image.imsave(path_crop_name, new_image)
