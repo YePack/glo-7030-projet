a44a0298887138d43128c574adb8a3e2d3e64bfd
Stephane Caron <ste.caron@icloud.com>
2020-01-03 17:21:12 -0500

    Add a module to split the dataset and make it compatible with TF dataset

---
+++ src/player_detection/data/data_folder.py
@@ -0,0 +1,85 @@
+import glob
+import os
+import math
+
+import numpy as np
+from shutil import copyfile
+
+from src.player_detection.xml_parser import xml_to_csv
+
+
+def create_data_folder(folder_from,
+                       folder_to,
+                       train_test_perc=0.8,
+                       train_valid_perc=0.8,
+                       shuffle=True):
+
+    # List all png files
+    images = glob.glob(folder_from + '*.png')
+
+    # List all XML files
+    xml = glob.glob(folder_from + '*.xml')
+
+    # Split train/valid/test
+    images.sort()
+    xml.sort()
+
+    if len(images) != len(xml):
+        print("You don't have the same number of .png and .xml files")
+        exit
+
+    nb_images = len(images)
+    indices = np.arange(nb_images)
+
+    if shuffle:
+        np.random.shuffle(indices)
+
+    split = math.floor(train_test_perc * nb_images)
+    train_idx, test_idx = indices[:split], indices[split:]
+
+    nb_images_train = len(train_idx)
+    indices_train = np.arange(nb_images_train)
+
+    if shuffle:
+        np.random.shuffle(indices_train)
+
+    split_train = math.floor(train_valid_perc * nb_images_train)
+    train_idx, valid_idx = indices_train[:split_train], indices_train[
+        split_train:]
+
+
+    # Create new folders for train and test datasets
+    os.mkdir(folder_to + 'train/')
+    os.mkdir(folder_to + 'valid/')
+    os.mkdir(folder_to + 'test/')
+
+    # Copy images files to right folders
+    for id in train_idx:
+        filename_png = images[id].split('/')[-1]
+        filename_xml = xml[id].split('/')[-1]
+        copyfile(images[id], os.path.join(folder_to, 'train', filename_png))
+        copyfile(xml[id], os.path.join(folder_to, 'train', filename_xml))
+    xml_to_csv(os.path.join(folder_to, 'train'),
+               output_file=os.path.join(folder_to, "train.csv"))
+
+    for id in valid_idx:
+        filename_png = images[id].split('/')[-1]
+        filename_xml = xml[id].split('/')[-1]
+        copyfile(images[id], os.path.join(folder_to, 'valid', filename_png))
+        copyfile(xml[id], os.path.join(folder_to, 'valid', filename_xml))
+    xml_to_csv(os.path.join(folder_to, 'valid'),
+               output_file=os.path.join(folder_to, "valid.csv"))
+
+    for id in test_idx:
+        filename_png = images[id].split('/')[-1]
+        filename_xml = xml[id].split('/')[-1]
+        copyfile(images[id], os.path.join(folder_to, 'test', filename_png))
+        copyfile(xml[id], os.path.join(folder_to, 'test', filename_xml))
+    xml_to_csv(os.path.join(folder_to, 'test'),
+               output_file=os.path.join(folder_to, "test.csv"))
+
+
+if __name__ == '__main__':
+    create_data_folder("/Users/stephanecaron/Downloads/test-xml/",
+                       "/Users/stephanecaron/Downloads/test-xml/data/",
+                       train_valid_perc=1)
--- src/player_detection/xml_parser.py
+++ src/player_detection/xml_parser.py
@@ -1,48 +1,50 @@
+import glob
 import os
 import xml.etree.ElementTree as ET
 import pandas as pd
 
-
-dtype = {
-    'classname': 'str',
-    'class': 'int'
-}
-dt_mapping = pd.read_csv('src/player_detection/data/classes.csv', dtype = dtype)
+dtype = {'classname': 'str', 'class': 'str'}
+dt_mapping = pd.read_csv('src/player_detection/data/classes.csv', dtype=dtype)
 
 
+def xml_to_csv(data_folder, output_file, dt_mapping=dt_mapping):
 
-def xml_to_csv(folder_from, save_to='src/player_detection/data/annotations.csv', dt_mapping=dt_mapping):
-    
-    xml_files = []
-    for files in os.listdir(folder_from):
-        if files.endswith('.xml'):
-            xml_files.append(files)
+    xml_files = glob.glob(data_folder + '/*.xml')
 
     # Initialize a pandas df
     dt_csv = pd.DataFrame(
-        columns=['image_filename', 'classname', 'x0', 'y0', 'x1', 'y1'])
+        columns=['image_id', 'classname', 'xmin', 'ymin', 'xmax', 'ymax'])
 
     # Loop through each XML file
     for xml_file in xml_files:
-    
-        tree = ET.parse(os.path.join(folder_from, xml_file))
+
+        tree = ET.parse(os.path.join(data_folder, xml_file))
         root = tree.getroot()
-    
+
         for image in root.iter('image'):
             for box in image.iter('box'):
                 dt_csv = dt_csv.append(
                     {
-                        'image_filename': image.attrib['name'],
+                        'image_id': image.attrib['name'],
+                        'xmin': int(float(box.attrib['xtl'])),
+                        'ymin': int(float(box.attrib['ytl'])),
+                        'xmax': int(float(box.attrib['xbr'])),
+                        'ymax': int(float(box.attrib['ybr'])),
                         'classname': box.attrib['label'].replace('-', '_'),
-                        'x0': int(float(box.attrib['xtl'])),
-                        'y0': int(float(box.attrib['ytl'])),
-                        'x1': int(float(box.attrib['xbr'])),
-                        'y1': int(float(box.attrib['ybr'])),
                     },
                     ignore_index=True)
 
     dt_csv = pd.merge(dt_csv, dt_mapping, on='classname', how='left')
-    dt_csv.to_csv(save_to, index=False)
+    dt_csv = dt_csv.astype({
+        "xmin": int,
+        "xmax": int,
+        "ymin": int,
+        "ymax": int
+    })
+    dt_csv = dt_csv.drop(columns=['classname'])
+    dt_csv.to_csv(output_file, index=False)
+
 
 if __name__ == '__main__':
-    xml_to_csv('/Users/stephanecaron/Downloads/test-xml/')
+    xml_to_csv('/Users/stephanecaron/Downloads/test-xml/',
+               output_file="test.csv")
