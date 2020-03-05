19fde5d4b9752184629acf9d2e5edcd9b38e0ed6
samperochkin <samuel.perreault.3@ulaval.ca>
2020-03-05 17:05:00 -0500

    homography loss v0

--- src/mapping/loss.py
+++ src/mapping/loss.py
@@ -5,23 +5,16 @@ import numpy as np
 from src.mapping.utils import warpPerspective, crop_center
 from src.semantic.utils.utils import readfile
 
-class DiceCoeff(nn.Module):
-    """Dice coeff for batch examples"""
+class MatchingLoss(nn.Module):
+
+    def __init__(self): 
+
+        rink_2d_label = readfile('data/rink/rink_label')
+        self.rink_2d = crop_center(rink_2d_label, 450, 256)
+        self.cen = np.array([256,128,1])
+
 
     def forward(self, input_label, h):
-        input_prime = warpPerspective(input_label, h)
-        rink_2d_label = readfile('data/rink/rink_label')
-        rink_2d_label_crop = crop_center(rink_2d_label, 450, 256)
-
-
-    @staticmethod
-    def losses(input_prime, plan_2d):
-        # first mean image second mean in plan
-        field_in_in = 0
-        field_in_out = 0
-        field_out_in = 0
-        field_out_out = 0
-        line = 0
-        circle = 0
-        for i in range
-
+        H = h.reshape(h.shape[0],3,3)
+        proj = warpPerspective(input_label, H)
+        -np.sum((proj == 1)*(self.rink_2d == 1)) + .001*(H.dot(self.cen)[1] - 128)**2
